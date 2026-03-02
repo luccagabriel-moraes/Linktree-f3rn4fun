1d16efe44232303f2d6c0bca8933c7c1afcf0429
luccagabriel-moraes <luccagabriel.moraes1@gmail.com>
2026-03-02 03:54:34 -0300

    inicial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function togglemode() {
+  const html = document.documentElement
+
+  html.classList.toggle("lighty")
+  /* if (html.classList.contains("lighty")) {
+    html.classList.remove("lighty")
+  } else {
+    html.classList.add("lighty")
+  } */
+}
